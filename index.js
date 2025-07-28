6f4822ca290bec9ed91283502bef5db3c36a0a32
BurcuMengu <burcumengu@gmail.com>
2025-07-28 15:47:33 +0300

    push

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-var dogAge = prompt("Write your age.");
+var dogAge = prompt("Write your dog age.");
 var humanAge = ((dogAge - 2) * 4 ) + 21;
 alert("Your dog is " + humanAge + " years old in human years.");
